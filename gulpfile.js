0139ee08fd45914cd1258c26a3483e7b25ca9cb1
Adam Ullman <adam@tokbox.com>
2014-09-03 16:43:48 +1000

    Fixes #1 we now have unit tests!

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     rename = require('gulp-rename'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    bower = require('gulp-bower');
 
-gulp.task('default', function(){
+gulp.task('bower', function () {
+  return bower();
+});
+
+gulp.task('default', ['bower'], function(){
     gulp.src('./opentok-layout.js')
         .pipe(jshint())
         .pipe(uglify({preserveComments: "some"}))
