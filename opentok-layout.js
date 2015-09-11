0dbcef9d06da5c6190c5d8d2cd2b6b4cf953573a
Adam Ullman <adam@tokbox.com>
2015-09-11 18:05:07 +1000

    Cleaned .jshint errors

--- opentok-layout.js
+++ opentok-layout.js
@@ -14,7 +14,7 @@
 // in browser globals context, ...? (when using bower, there are dependencies that it has handled
 // for you, so these might be safe to assume)
 
-
+/*globals jQuery */
 if (typeof module === 'undefined' || typeof module.exports === 'undefined') {
   exports = window;
 }
@@ -22,42 +22,44 @@ if (typeof module === 'undefined' || typeof module.exports === 'undefined') {
 (function($) {
     var positionElement = function positionElement(elem, x, y, width, height, animate) {
         var targetPosition = {
-            left: x + "px",
-            top: y + "px",
-            width: width + "px",
-            height: height + "px"
+            left: x + 'px',
+            top: y + 'px',
+            width: width + 'px',
+            height: height + 'px'
         };
 
         var fixAspectRatio = function () {
-            var sub = elem.querySelector(".OT_root");
+            var sub = elem.querySelector('.OT_root');
             if (sub) {
                 // If this is the parent of a subscriber or publisher then we need
                 // to force the mutation observer on the publisher or subscriber to
                 // trigger to get it to fix it's layout
                 var oldWidth = sub.style.width;
-                sub.style.width = width + "px";
-                // sub.style.height = height + "px";
-                sub.style.width = oldWidth || "";
+                sub.style.width = width + 'px';
+                // sub.style.height = height + 'px';
+                sub.style.width = oldWidth || '';
             }
         };
 
         if (animate && $) {
             console.log('animate');
             $(elem).stop();
-            $(elem).animate(targetPosition, animate.duration || 200, animate.easing || "swing", function () {
+            $(elem).animate(targetPosition, animate.duration || 200, animate.easing || 'swing',
+              function () {
                 fixAspectRatio();
                 if (animate.complete) animate.complete.call(this);
             });
         } else {
-            // NOTE: internal OT.$ API
-            OT.$.css(elem, targetPosition);
+            elem.style.left = targetPosition.left;
+            elem.style.top = targetPosition.top;
+            elem.style.width = targetPosition.width;
+            elem.style.height = targetPosition.height;
         }
         fixAspectRatio();
     };
 
     var getCSSNumber = function (elem, prop) {
-        // NOTE: internal OT.$ API
-        var cssStr = OT.$.css(elem, prop);
+        var cssStr = window.getComputedStyle(elem)[prop];
         return cssStr ? parseInt(cssStr, 10) : 0;
     };
 
@@ -67,20 +69,33 @@ if (typeof module === 'undefined' || typeof module.exports === 'undefined') {
     };
 
     var getHeight = function (elem) {
-        // NOTE: internal OT.$ API
-        var heightStr = OT.$.height(elem);
-        return heightStr ? parseInt(heightStr, 10) : 0;
+      return elem.clientHeight;
     };
 
     var getWidth = function (elem) {
-        // NOTE: internal OT.$ API
-        var widthStr = OT.$.width(elem);
-        return widthStr ? parseInt(widthStr, 10) : 0;
+        return elem.clientWidth;
     };
 
-    var arrange = function arrange(children, Width, Height, offsetLeft, offsetTop, fixedRatio, minRatio, maxRatio, animate) {
+    // Use default values if it is not already set in val
+    var defaults = function (val, def) {
+      var res = {},
+        key;
+      for (key in val) {
+        if (val.hasOwnProperty(key)) {
+          res[key] = val[key];
+        }
+      }
+      for (key in def) {
+        if (def.hasOwnProperty(key) && !res.hasOwnProperty(key)) {
+          res[key] = def[key];
+        }
+      }
+      return res;
+    };
+
+    var arrange = function arrange(children, Width, Height, offsetLeft, offsetTop, fixedRatio,
+      minRatio, maxRatio, animate) {
         var count = children.length,
-            availableRatio = Height / Width,
             vidRatio;
 
         var getBestDimensions = function getBestDimensions(minRatio, maxRatio) {
@@ -102,7 +117,7 @@ if (typeof module === 'undefined' || typeof module.exports === 'undefined') {
                 tHeight = Math.floor( Height/rows );
                 tWidth = Math.floor(Width/cols);
 
-                tRatio = tHeight/tWidth;
+                var tRatio = tHeight/tWidth;
                 if (tRatio > maxRatio) {
                     // We went over decrease the height
                     tRatio = maxRatio;
@@ -138,7 +153,7 @@ if (typeof module === 'undefined' || typeof module.exports === 'undefined') {
             vidRatio = getBestDimensions(minRatio, maxRatio);
         } else {
             // Use the ratio of the first video element we find
-            var video = children.length > 0 && children[0].querySelector("video");
+            var video = children.length > 0 && children[0].querySelector('video');
             if (video && video.videoHeight && video.videoWidth) {
                 vidRatio = getBestDimensions(video.videoHeight/video.videoWidth,
                     video.videoHeight/video.videoWidth);
@@ -162,68 +177,66 @@ if (typeof module === 'undefined' || typeof module.exports === 'undefined') {
             if (i % vidRatio.targetCols === 0) {
                 // We are the first element of the row
                 x = firstColMarginLeft;
-                if (i == lastRowIndex) x += lastRowMargin;
+                if (i === lastRowIndex) x += lastRowMargin;
                 y += i === 0 ? firstRowMarginTop : vidRatio.targetHeight;
             } else {
                 x += vidRatio.targetWidth;
             }
 
-            // NOTE: internal OT.$ API
-            OT.$.css(elem, "position", "absolute");
-            var actualWidth = vidRatio.targetWidth - getCSSNumber(elem, "paddingLeft") -
-                            getCSSNumber(elem, "paddingRight") -
-                            getCSSNumber(elem, "marginLeft") -
-                            getCSSNumber(elem, "marginRight") -
-                            getCSSNumber(elem, "borderLeft") -
-                            getCSSNumber(elem, "borderRight");
+            elem.style.position = 'absolute';
+            var actualWidth = vidRatio.targetWidth - getCSSNumber(elem, 'paddingLeft') -
+                            getCSSNumber(elem, 'paddingRight') -
+                            getCSSNumber(elem, 'marginLeft') -
+                            getCSSNumber(elem, 'marginRight') -
+                            getCSSNumber(elem, 'borderLeft') -
+                            getCSSNumber(elem, 'borderRight');
 
-             var actualHeight = vidRatio.targetHeight - getCSSNumber(elem, "paddingTop") -
-                            getCSSNumber(elem, "paddingBottom") -
-                            getCSSNumber(elem, "marginTop") -
-                            getCSSNumber(elem, "marginBottom") -
-                            getCSSNumber(elem, "borderTop") -
-                            getCSSNumber(elem, "borderBottom");
+             var actualHeight = vidRatio.targetHeight - getCSSNumber(elem, 'paddingTop') -
+                            getCSSNumber(elem, 'paddingBottom') -
+                            getCSSNumber(elem, 'marginTop') -
+                            getCSSNumber(elem, 'marginBottom') -
+                            getCSSNumber(elem, 'borderTop') -
+                            getCSSNumber(elem, 'borderBottom');
 
             positionElement(elem, x+offsetLeft, y+offsetTop, actualWidth, actualHeight, animate);
         }
     };
 
-    var filterDisplayNone = function (element) {
-        // NOTE: internal OT.$ API
-        return OT.$.css(element, "display") !== "none";
+    var isDisplayed = function (element) {
+        return window.getComputedStyle(element).display !== 'none';
     };
 
-    var layout = function layout(container, opts, fixedRatio) {
-        // NOTE: internal OT.$ API
-        if (OT.$.css(container, "display") === "none") {
-            return;
+    var layout = function layout(container, opts) {
+        if (!isDisplayed(container)) {
+          return;
         }
-        var id = container.getAttribute("id");
+        var id = container.getAttribute('id');
         if (!id) {
-            id = "OT_" + cheapUUID();
-            container.setAttribute("id", id);
+            id = 'OT_' + cheapUUID();
+            container.setAttribute('id', id);
         }
 
         var Height = getHeight(container) -
-                    getCSSNumber(container, "borderTop") -
-                    getCSSNumber(container, "borderBottom"),
+                    getCSSNumber(container, 'borderTop') -
+                    getCSSNumber(container, 'borderBottom'),
             Width = getWidth(container) -
-                    getCSSNumber(container, "borderLeft") -
-                    getCSSNumber(container, "borderRight"),
+                    getCSSNumber(container, 'borderLeft') -
+                    getCSSNumber(container, 'borderRight'),
             availableRatio = Height/Width,
             offsetLeft = 0,
             offsetTop = 0,
             bigOffsetTop = 0,
             bigOffsetLeft = 0,
+            bigRatio,
             bigOnes = Array.prototype.filter.call(
-                container.querySelectorAll("#" + id + ">." + opts.bigClass),
-                filterDisplayNone),
+                container.querySelectorAll('#' + id + '>.' + opts.bigClass),
+                isDisplayed),
             smallOnes = Array.prototype.filter.call(
-                container.querySelectorAll("#" + id + ">*:not(." + opts.bigClass + ")"),
-                filterDisplayNone);
+                container.querySelectorAll('#' + id + '>*:not(.' + opts.bigClass + ')'),
+                isDisplayed);
 
         if (bigOnes.length > 0 && smallOnes.length > 0) {
-            var bigVideo = bigOnes[0].querySelector("video");
+            var bigVideo = bigOnes[0].querySelector('video');
             if (bigVideo && bigVideo.videoHeight && bigVideo.videoWidth) {
                 bigRatio = bigVideo.videoHeight / bigVideo.videoWidth;
             } else {
@@ -232,63 +245,61 @@ if (typeof module === 'undefined' || typeof module.exports === 'undefined') {
             var bigWidth, bigHeight;
 
             if (availableRatio > bigRatio) {
-                // We are tall, going to take up the whole width and arrange small guys at the bottom
+                // We are tall, going to take up the whole width and arrange
+                // small guys at the bottom
                 bigWidth = Width;
                 bigHeight = Math.min(Math.floor(Height * opts.bigPercentage), Width * bigRatio);
                 offsetTop = bigHeight;
                 bigOffsetTop = Height - offsetTop;
             } else {
-                // We are wide, going to take up the whole height and arrange the small guys on the right
+                // We are wide, going to take up the whole height and arrange the small guys
+                // on the right
                 bigHeight = Height;
                 bigWidth = Math.min(Width * opts.bigPercentage, Math.floor(bigHeight / bigRatio));
                 offsetLeft = bigWidth;
                 bigOffsetLeft = Width - offsetLeft;
             }
             if (opts.bigFirst) {
-              arrange(bigOnes, bigWidth, bigHeight, 0, 0, opts.bigFixedRatio, opts.bigMinRatio, opts.bigMaxRatio, opts.animate);
-              arrange(smallOnes, Width - offsetLeft, Height - offsetTop, offsetLeft, offsetTop, opts.fixedRatio, opts.minRatio, opts.maxRatio, opts.animate);
+              arrange(bigOnes, bigWidth, bigHeight, 0, 0, opts.bigFixedRatio, opts.bigMinRatio,
+                opts.bigMaxRatio, opts.animate);
+              arrange(smallOnes, Width - offsetLeft, Height - offsetTop, offsetLeft, offsetTop,
+                opts.fixedRatio, opts.minRatio, opts.maxRatio, opts.animate);
             } else {
-              arrange(smallOnes, Width - offsetLeft, Height - offsetTop, 0, 0, opts.fixedRatio, opts.minRatio, opts.maxRatio, opts.animate);
-              arrange(bigOnes, bigWidth, bigHeight, bigOffsetLeft, bigOffsetTop, opts.bigFixedRatio, opts.bigMinRatio, opts.bigMaxRatio, opts.animate);
+              arrange(smallOnes, Width - offsetLeft, Height - offsetTop, 0, 0, opts.fixedRatio,
+                opts.minRatio, opts.maxRatio, opts.animate);
+              arrange(bigOnes, bigWidth, bigHeight, bigOffsetLeft, bigOffsetTop, opts.bigFixedRatio,
+                opts.bigMinRatio, opts.bigMaxRatio, opts.animate);
             }
         } else if (bigOnes.length > 0 && smallOnes.length === 0) {
             // We only have one bigOne just center it
-            arrange(bigOnes, Width, Height, 0, 0, opts.bigFixedRatio, opts.bigMinRatio, opts.bigMaxRatio, opts.animate);
+            arrange(bigOnes, Width, Height, 0, 0, opts.bigFixedRatio, opts.bigMinRatio,
+              opts.bigMaxRatio, opts.animate);
         } else {
-            arrange(smallOnes, Width - offsetLeft, Height - offsetTop, offsetLeft, offsetTop, opts.fixedRatio, opts.minRatio, opts.maxRatio, opts.animate);
+            arrange(smallOnes, Width - offsetLeft, Height - offsetTop, offsetLeft, offsetTop,
+              opts.fixedRatio, opts.minRatio, opts.maxRatio, opts.animate);
         }
     };
 
     exports.initLayoutContainer = function(container, opts) {
-        // NOTE: internal OT.$ API
-        opts = OT.$.defaults(opts || {}, {
+        opts = defaults(opts || {}, {
             maxRatio: 3/2,
             minRatio: 9/16,
             fixedRatio: false,
             animate: false,
-            bigClass: "OT_big",
+            bigClass: 'OT_big',
             bigPercentage: 0.8,
             bigFixedRatio: false,
             bigMaxRatio: 3/2,
             bigMinRatio: 9/16,
             bigFirst: true
         });
-        // NOTE: internal OT.$ API
-        container = typeof(container) == "string" ? OT.$(container) : container;
+        container = typeof(container) === 'string' ? document.querySelector(container) : container;
 
-        // TODO: should we add event hooks to external globals like this?
-        // this could be left as a responsibility of the user, and i think that would be more sound
-        // the OT.onLoad() method has (publicly) undefined behavior
-        OT.onLoad(function() {
-            layout(container, opts);
-        });
+        layout(container, opts);
 
         return {
             layout: layout.bind(null, container, opts)
         };
     };
-
-    // NOTE: deprecated API, will be removed in next major version
-    OT.initLayoutContainer = exports.initLayoutContainer;
 
 })(window.hasOwnProperty('jQuery') ? jQuery : undefined);
